088078da5c879756b295eb77641cb80a99f3c91b
Abdulla Al Harun <abdhar01276@stud.noroff.no>
2024-03-04 03:07:49 +0100

    added post Dinamically from API!

---
+++ js/app.js
@@ -0,0 +1,67 @@
+const loadPost = async () => {
+    const res = await fetch('https://openapi.programming-hero.com/api/retro-forum/posts');
+    const data = await res.json();
+    const posts = data.posts;
+
+    displayPost(posts);
+}
+
+const displayPost = posts => {
+    const postContainer = document.getElementById('post-container');
+
+    posts.forEach(post => {
+        const postCard = document.createElement('div');
+        postCard.classList = `rounded-xl border-2 p-6 bg-gray-300 card pb-8`;
+        postCard.innerHTML = `
+            <div class="card-container flex text-center gap-8">
+                <div class="image-div">
+                    <div>
+                        <img class="rounded-2xl w-16 h-16" src="${post.image}">
+                    </div>
+                    <div class="circle-div">
+                        <i class="fa-solid fa-circle" id="circle-icon-${post.id}"></i>
+                    </div>
+                </div>
+                <div>
+                    <div class="flex gap-4">
+                        <p># <span id="category">${post.category}</span></p>
+                        <p>Author: <span id="author">${post.author.name}</span></p>
+                    </div>
+                    <h2 class="text-xl text-start font-extrabold text-heading py-4">${post.title}</h2>
+                    <div> 
+                        <p class="text-gray-600 text-start">${post.description}</p>
+                    </div>
+                    
+                    <hr class="border-dashed pt-4 pb-4">
+                    
+                    <div class="flex justify-between">
+                        <p>
+                            <span class="pr-4"><i class="fa-regular fa-message pr-2"></i></i>${post.comment_count}</span>
+                            <span class="pr-4"><i class="fa-solid fa-eye pr-2"></i>${post.view_count}</span>
+                            <span><i class="fa-regular fa-clock pr-2"></i>${post.posted_time}</span>
+                        </p>
+                        <p>
+                            <button id="read-done" class="btn btn-circle"><i class="fa-solid fa-envelope-circle-check text-green-500"></i></button>
+                        </p>
+                    </div>
+                </div>
+            </div>
+        `;
+
+        // Append the postCard to the container
+        postContainer.appendChild(postCard);
+
+        // Get the circleIcon for this post
+        var circleIcon = document.getElementById(`circle-icon-${post.id}`);
+        var isActive = post.isActive;
+
+        // Add class based on the value of isActive
+        if (isActive) {
+            circleIcon.classList.add("fa-circle-green");
+        } else {
+            circleIcon.classList.add("fa-circle-red");
+        }
+    })
+}
+
+loadPost();
